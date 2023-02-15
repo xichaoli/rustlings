6c1aba4f61578605cfc7aacc9ba4b2625ffc1ca3
xichaoli <xichao@sina.cn>
2023-02-15 22:33:11 +0800

    完成 hashmap

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -11,7 +11,7 @@
 //
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// 用match如何实现?
 
 use std::collections::HashMap;
 
@@ -34,9 +34,7 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
-        // TODO: Put new fruits if not already present. Note that you
-        // are not allowed to put any type of fruit that's already
-        // present!
+        basket.entry(fruit).or_insert(3);
     }
 }
 
