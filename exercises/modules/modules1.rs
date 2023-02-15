8fcc201a4de095ed8f29240880bd88ef56d98342
xichaoli <xichao@sina.cn>
2023-02-15 22:13:05 +0800

    完成 modules

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -1,7 +1,8 @@
 // modules1.rs
 // Execute `rustlings hint modules1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// 默认情况下，模块中的项拥有私有的可见性（private visibility）
+// 项可以是：函数，结构体，trait，impl 块，甚至其它模块。
 
 mod sausage_factory {
     // Don't let anybody outside of this module see this!
@@ -9,7 +10,7 @@ mod sausage_factory {
         String::from("Ginger")
     }
 
-    fn make_sausage() {
+    pub fn make_sausage() {
         get_secret_recipe();
         println!("sausage!");
     }
