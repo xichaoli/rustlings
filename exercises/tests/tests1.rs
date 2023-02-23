c37a2e1ceeb067f6424b64ed2289283998ff5216
xichaoli <xichao@sina.cn>
2023-02-23 14:33:55 +0800

    完成 tests

--- exercises/tests/tests1.rs
+++ exercises/tests/tests1.rs
@@ -7,12 +7,18 @@
 // pass! Make the test fail!
 // Execute `rustlings hint tests1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// assert!()
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert() {
-        assert!();
+        assert!(true);
+    }
+
+    #[test]
+    #[should_panic]
+    fn you_can_not_assert() {
+        assert!(false);
     }
 }
--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -3,12 +3,17 @@
 // pass! Make the test fail!
 // Execute `rustlings hint tests2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// assert_eq!()
+// assert_ne!()
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        assert_eq!(3, 3);
+    }
+
+    fn you_can_not_assert_eq() {
+        assert_ne!(3, 5);
     }
 }
