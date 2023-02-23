da4df0c3d13c9ddf6295e42b64971a9e98a5d625
xichaoli <xichao@sina.cn>
2023-02-23 16:51:42 +0800

    完成 lifetime

--- exercises/lifetimes/lifetimes1.rs
+++ exercises/lifetimes/lifetimes1.rs
@@ -7,9 +7,9 @@
 //
 // Execute `rustlings hint lifetimes1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// longest<'a> : longest() 的生命周期要小于等于'a
 
-fn longest(x: &str, y: &str) -> &str {
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
