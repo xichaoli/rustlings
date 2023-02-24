b26b63dfcb01ec3c9cb88b0b864989f87d68e165
xichaoli <xichao@sina.cn>
2023-02-24 15:00:25 +0800

    完成 thread, 还需加深理解

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -6,7 +6,6 @@
 // The program should wait until all the spawned threads have finished and
 // should collect their return values into a vector.
 
-// I AM NOT DONE
 
 use std::thread;
 use std::time::{Duration, Instant};
@@ -21,10 +20,16 @@ fn main() {
             start.elapsed().as_millis()
         }));
     }
+    // 这时 handles 内容如下：
+    // [JoinHandle { .. }, JoinHandle { .. }, JoinHandle { .. }, JoinHandle { .. },
+    // JoinHandle { .. }, JoinHandle { .. }, JoinHandle { .. }, JoinHandle { .. },
+    // JoinHandle { .. }, JoinHandle { .. }]
 
     let mut results: Vec<u128> = vec![];
     for handle in handles {
         // TODO: a struct is returned from thread::spawn, can you use it?
+        // handle执行完成后，返回值被push到results
+        results.push(handle.join().unwrap());
     }
 
     if results.len() != 10 {
--- exercises/threads/threads2.rs
+++ exercises/threads/threads2.rs
@@ -3,9 +3,16 @@
 // Building on the last exercise, we want all of the threads to complete their work but this time
 // the spawned threads need to be in charge of updating a shared value: JobStatus.jobs_completed
 
-// I AM NOT DONE
+// status_shared.lock().unwrap()
+// status_shared 的类型是 Arc<Mutex<JobStatus>
+// .lock() 后的类型 怎么变成了 LockResult<MutexGuard<JobStatus>> ?
+// Arc 呢 ？
+// 答案在 let status_shared = Arc::clone(&status);
+// 在 Arc 上调用 clone 会生成一个新的 Arc 实例，
+// 该实例指向堆上与源 Arc 相同的分配，同时增加了引用计数。
+// lock() 是作用在 Mutex<JobStatus> 上，返回了 LockResult<MutexGuard<JobStatus>>
 
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -14,21 +21,27 @@ struct JobStatus {
 }
 
 fn main() {
-    let status = Arc::new(JobStatus { jobs_completed: 0 });
+    let status = Arc::new(Mutex::new(JobStatus { jobs_completed: 0 }));
     let mut handles = vec![];
     for _ in 0..10 {
         let status_shared = Arc::clone(&status);
         let handle = thread::spawn(move || {
             thread::sleep(Duration::from_millis(250));
-            // TODO: You must take an action before you update a shared value
-            status_shared.jobs_completed += 1;
+            let mut counter = status_shared
+                .lock()
+                .unwrap();
+            counter.jobs_completed += 1;
         });
         handles.push(handle);
     }
     for handle in handles {
         handle.join().unwrap();
-        // TODO: Print the value of the JobStatus.jobs_completed. Did you notice anything
         // interesting in the output? Do you have to 'join' on all the handles?
-        println!("jobs completed {}", ???);
+        let completed = status
+            .lock()
+            .unwrap()
+            .jobs_completed;
+
+        println!("jobs completed {}", completed);
     }
 }
