a2421a30b0c0174565ad1a451f4aceadc1b078c2
xichaoli <xichao@sina.cn>
2023-02-24 17:25:18 +0800

    完成 clippy.

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -1,13 +1,14 @@
 // clippy2.rs
 // Execute `rustlings hint clippy2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// 莫名其妙
 
 fn main() {
     let mut res = 42;
     let option = Some(12);
-    for x in option {
+    // for x in option { // 莫名其妙 的 for
+    if let Some(x) = option {
         res += x;
     }
-    println!("{}", res);
+    println!("{res}");
 }
